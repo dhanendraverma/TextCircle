92e26e77c8d9d6a148d4f6e227fe2e45853f992f
unknown <v.dhanendra@gmail.com>
2019-07-24 01:27:34 +0530

    started the app structure

---
+++ textcircle.js
@@ -0,0 +1,29 @@
+this.Documents = new Mongo.Collection("documents");
+
+
+if (Meteor.isClient){
+	Template.editor.helpers({
+		docid:function(){
+			console.log("doc id helper:");
+			console.log(Documents.findOne());
+			var doc = Documents.findOne();
+			if(doc) {
+				return doc._id;
+			}
+			else {
+				return undefined;
+			}
+		}
+	});
+
+}
+
+if (Meteor.isServer){
+	Meteor.startup(function(){
+		// code to run on server at startup
+		if(!Documents.findOne()) { //no documents yet
+			Documents.insert({title:"my new document"});
+
+		}
+	})
+}
