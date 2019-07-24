0eca18164ba589ddb8196914f52c6202eb8f6153
unknown <v.dhanendra@gmail.com>
2019-07-24 12:42:37 +0530

    bootstrapped it and added nav bar

--- textcircle.js
+++ textcircle.js
@@ -2,17 +2,6 @@ this.Documents = new Mongo.Collection("documents");
 
 
 if (Meteor.isClient){
-	//update the session current_date
-	//variable every 1000 ms
-	Meteor.setInterval(function(){
-		Session.set("current_date", new Date());
-	},1000);
-
-	Template.date_display.helpers({
-		current_date:function(){
-			return Session.get("current_date");
-		}
-	});
 
 	Template.editor.helpers({
 		docid:function(){
