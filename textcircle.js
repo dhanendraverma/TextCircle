5ed9dc2e56a02a2c0e3841691ed4526532594147
unknown <v.dhanendra@gmail.com>
2019-07-24 22:16:23 +0530

    added userlogin option

--- textcircle.js
+++ textcircle.js
@@ -1,37 +1,36 @@
 this.Documents = new Mongo.Collection("documents");
 
-
-if (Meteor.isClient){
-
-	Template.editor.helpers({
-		docid:function(){
-			console.log("doc id helper:");
-			console.log(Documents.findOne());
-			var doc = Documents.findOne();
-			if(doc) {
-				return doc._id;
-			}
-			else {
-				return undefined;
-			}
-		}
-		config:function(editor){
-			return function(editor){
-				editor.on("change", function(cm_editor, info))
-				console.log(cm_editor.getValue());
-				$('#viewer_iframe').contents().find("html").html(cm_editor.getValue());
-			}
-		}
-	});
-
+if (Meteor.isClient) {
+// find the first document in the Documents colleciton and send back its id
+  Template.editor.helpers({
+    docid:function(){
+      var doc = Documents.findOne();
+      if (doc){
+        return doc._id;
+      }
+      else {
+        return undefined;
+      }
+    }, 
+    // template helper that configures the CodeMirror editor
+    // you might also want to experiment with the ACE editor
+    config:function(){
+      return function(editor){
+        editor.setOption("mode", "html");
+        editor.on("change", function(cm_editor, info){
+          //console.log(cm_editor.getValue());
+          $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
+        });        
+      }
+    }, 
+  });
 }
 
-if (Meteor.isServer){
-	Meteor.startup(function(){
-		// code to run on server at startup
-		if(!Documents.findOne()) { //no documents yet
-			Documents.insert({title:"my new document"});
-
-		}
-	})
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // startup code that creates a document in case there isn't one yet. 
+    if (!Documents.findOne()){// no documents yet!
+        Documents.insert({title:"my new document"});
+    }
+  });
 }
