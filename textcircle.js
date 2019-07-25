dac4805ccc4b93c9a11958b332c51a558fa11d53
unknown <v.dhanendra@gmail.com>
2019-07-25 16:37:54 +0530

    update the DB and seperate client account

--- textcircle.js
+++ textcircle.js
@@ -1,8 +1,10 @@
+// this collection stores all the documents 
 this.Documents = new Mongo.Collection("documents");
+// this collection stores sets of users that are editing documents
 EditingUsers = new Mongo.Collection("editingUsers");
 
 if (Meteor.isClient) {
-// find the first document in the Documents colleciton and send back its id
+  // return the id of the first document you can find
   Template.editor.helpers({
     docid:function(){
       var doc = Documents.findOne();
@@ -13,14 +15,15 @@ if (Meteor.isClient) {
         return undefined;
       }
     }, 
-    // template helper that configures the CodeMirror editor
-    // you might also want to experiment with the ACE editor
+    // configure the CodeMirror editor
     config:function(){
       return function(editor){
-        editor.setOption("lineNumber", true);
-        editor.setOption("theme","cobalt");
+        editor.setOption("lineNumbers", true);
+        editor.setOption("theme", "cobalt");
+        // set a callback that gets triggered whenever the user
+        // makes a change in the code editing window
         editor.on("change", function(cm_editor, info){
-          //console.log(cm_editor.getValue());
+          // send the current code over to the iframe for rendering
           $("#viewer_iframe").contents().find("html").html(cm_editor.getValue());
           Meteor.call("addEditingUser");
         });        
@@ -29,49 +32,64 @@ if (Meteor.isClient) {
   });
 
   Template.editingUsers.helpers({
-  	users:function(){
-  		var doc, eusers, users;
-  		doc = Documents.findOne();
-  		if(!doc){return;} //no doc
-  		eusers = EditingUsers.findOne({docid:doc._id});
-  		if(!eusers){return;} //no users are editing currently
-  		users = new Array();
-  		var i = 0;
-  		for(var user_id in eusers.users){
-  			users[i] = eusers.users[user_id];
-  			i++;
-  		}
-  		return users;
-  	}
+    // retrieve a set of users that are editing this document
+    users:function(){
+      var doc, eusers, users;
+      doc = Documents.findOne();
+      if (!doc){return;}// give up
+      eusers = EditingUsers.findOne({docid:doc._id});
+      if (!eusers){return;}// give up
+      users = new Array();
+      var i = 0;
+      for (var user_id in eusers.users){
+          users[i] = fixObjectKeys(eusers.users[user_id]);
+          i++;
+      }
+      return users;
+    }
   })
-}//end of is isClient block
+ 
+}// end isClient...
 
 if (Meteor.isServer) {
   Meteor.startup(function () {
-    // startup code that creates a document in case there isn't one yet. 
+    // insert a document if there isn't one already
     if (!Documents.findOne()){// no documents yet!
         Documents.insert({title:"my new document"});
     }
   });
 }
-
+// methods that provide write access to the data
 Meteor.methods({
-	addEditingUser:function(){
-		var doc, user, eusers;
-		doc = Documents.findOne();
-		if(!doc){ return; } //no doc give up
-		if(!this.userId){return;} //no logged in user give up
-		//now we have a doc and possibly a user
-		user = Meteor.user().profile;
-		eusers = EditingUsers.findOne({docid:doc._id});
-		if(!eusers){
-			eusers = {
-				docid:doc._id,
-				users:{},
-			};
-		}
-		user.lastEdit = new Date();
-		eusers.users[this.userId] = user;
-		EditingUsers.upsert({_id:eusers._id},eusers);
-	}
+  // allows changes to the editing users collection 
+  addEditingUser:function(){
+    var doc, user, eusers;
+    doc = Documents.findOne();
+    if (!doc){return;}// no doc give up
+    if (!this.userId){return;}// no logged in user give up
+    // now I have a doc and possibly a user
+    user = Meteor.user().profile;
+    eusers = EditingUsers.findOne({docid:doc._id});
+    if (!eusers){// no editing users have been stored yet
+      eusers = {
+        docid:doc._id, 
+        users:{}, 
+      };
+    }
+    user.lastEdit = new Date();
+    eusers.users[this.userId] = user;
+    // upsert- insert or update if filter matches
+    EditingUsers.upsert({_id:eusers._id}, eusers);
+  }
 })
+
+// this renames object keys by removing hyphens to make the compatible 
+// with spacebars. 
+function fixObjectKeys(obj){
+  var newObj = {};
+  for (key in obj){
+    var key2 = key.replace("-", "");
+    newObj[key2] = obj[key];
+  }
+  return newObj;
+}
